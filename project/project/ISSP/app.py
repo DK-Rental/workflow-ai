0c311c9345acc7495210111010b21a373750dc5f
Jin Siang Teh <jteh1@my.bcit.ca>
2026-05-18 13:03:50 -0700

    AI-search upgrade

--- project/project/ISSP/app.py
+++ project/project/ISSP/app.py
@@ -361,13 +361,19 @@ async def on_turn(turn_context: TurnContext):
 def messages():
     if "application/json" not in request.headers.get("Content-Type", ""):
         return Response(status=415)
-    activity = Activity().deserialize(request.json)
+
+    activity    = Activity().deserialize(request.json)
+    auth_header = request.headers.get("Authorization", "")
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(on_turn(TurnContext(BOT_ADAPTER, activity)))
+        loop.run_until_complete(
+            BOT_ADAPTER.process_activity(activity, auth_header, on_turn)
+        )
     finally:
         loop.close()
+
     return Response(status=200)
 
 
